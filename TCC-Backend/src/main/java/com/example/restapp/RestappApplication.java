307f534ff51f28eb492ed3a83d1f733a934f9f74
Antony Rafael <antony05rafael@gmail.com>
2025-11-25 10:31:00 -0300

    Remoção de auditoria colocada acidentalmente no lugar errado

--- TCC-Backend/src/main/java/com/example/restapp/RestappApplication.java
+++ TCC-Backend/src/main/java/com/example/restapp/RestappApplication.java
@@ -2,10 +2,8 @@ package com.example.restapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class RestappApplication
 {
 
